c270004edb4473acecbb415f8fb1e7f8df6a698c
LasindaDilshan <32431966+LasindaDilshan@users.noreply.github.com>
2023-03-19 19:36:10 +0530

    Add project files.

---
+++ CleanMovie.Infrastructure/MovieRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanMovie.Application;
+using CleanMovieDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanMovie.Infrastructure
+{
+    public class MovieRepository : IMovieRepository
+    {
+        public static List<Movie> movies = new List<Movie>()
+        {
+            new Movie{ Id = 1 , Name = "Lord Of The rings" , Cost = 2} ,
+             new Movie{ Id = 1 , Name = "Magic Elephent" , Cost = 1}
+        };
+
+        public List<Movie> GetAllMovies()
+        {
+            return movies;
+        }
+    }
+}
